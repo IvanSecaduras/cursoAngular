c986b8d648ed14729930919f91d60f76ab8bf56f
Iván Secaduras Cano <ivansecaduras@outlook.com>
2019-10-12 20:34:47 +0200

    Aplicación de mapas utilizando API de Google Maps. Uso de Angular Material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { AgmCoreModule } from '@agm/core';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
+  entryComponents: [
+    MapaEditarComponent
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA6xoyYRHRM5inU57sXrgRRCdT1hzTatwE'
+    }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,8 @@
+export class Marcador {
+
+  public titulo:string = 'Sin título';
+  public desc:string = 'Sin descripción';
+
+  constructor(public lat:number, public lng:number) {}
+
+}
